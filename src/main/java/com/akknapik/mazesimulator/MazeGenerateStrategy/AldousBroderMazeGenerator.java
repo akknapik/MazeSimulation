d4d8c2bac3adb5a85d2be4b85bad0823107e4f55
akknapik <arkadiusz.knapik.2002@gmail.com>
2025-01-01 23:35:06 +0100

    feat: add AldousBroderMazeGenerator implementation

--- src/main/java/com/akknapik/mazesimulator/MazeGenerateStrategy/AldousBroderMazeGenerator.java
+++ src/main/java/com/akknapik/mazesimulator/MazeGenerateStrategy/AldousBroderMazeGenerator.java
@@ -1,4 +1,33 @@
 package com.akknapik.mazesimulator.MazeGenerateStrategy;
 
-public class AldousBroderMazeGenerator {
+import com.akknapik.mazesimulator.Cell;
+import com.akknapik.mazesimulator.Maze;
+import com.akknapik.mazesimulator.State.UnvisitedState;
+import com.akknapik.mazesimulator.State.VisitedState;
+
+import java.util.List;
+import java.util.Random;
+
+public class AldousBroderMazeGenerator extends MazeGeneratorStrategy implements IMazeGeneratorStrategy{
+    @Override
+    public void generateMaze(Cell[][] grid) {
+        int sizeOfMaze = grid.length;
+        Cell current = grid[new Random().nextInt(sizeOfMaze)][new Random().nextInt(sizeOfMaze)];
+        current.setVisited(new VisitedState());
+        int visitedCounter = 1;
+        int numberOfCells = sizeOfMaze * sizeOfMaze;
+
+        while (visitedCounter < numberOfCells) {
+            List<Cell> neighbors = getNeighbors(current, grid);
+            Cell neighbor = neighbors.get(new Random().nextInt(neighbors.size()));
+
+            if(!neighbor.getVisited().isVisited()) {
+                removeWall(current, neighbor);
+                neighbor.setVisited(new VisitedState());
+                visitedCounter++;
+            }
+            //visitedCounter++;
+            current = neighbor;
+        }
+    }
 }
